4e272530eac97db80e4bdf147bba7459f22019e4
D3V-D <devendangi10@gmail.com>
2023-10-10 21:37:13 -0400

    Updated example map to center on Tall.

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([30.4380832, -84.2809332], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
