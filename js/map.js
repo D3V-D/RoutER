971507bce1625515daebe99c50de585a1eae7822
D3V-D <devendangi10@gmail.com>
2023-10-19 20:05:57 -0400

    Added location detection, sidebar on map, and routER logo

--- js/map.js
+++ js/map.js
@@ -4,3 +4,54 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+L.control.scale().addTo(map);
+
+var current_position, current_accuracy;
+
+var initialGeolocation = true;
+var currentlyTracking = true;
+
+function onLocationFound(e) {
+    
+    // if we have current position, remove old pos.
+    if (current_position) {
+        map.removeLayer(current_position);
+        map.removeLayer(current_accuracy);
+    }
+
+    var radius = e.coords.accuracy / 10;
+
+    const latlng = {
+        lat: e.coords.latitude,
+        lng: e.coords.longitude
+    }
+
+    if (initialGeolocation) {
+        initialGeolocation = false;
+        map.setZoom(17);
+        alert("Location may be inaccurate; if so, please enter start address manually. Please wait a few seconds first.");
+        map.setView(latlng);
+    }
+
+    current_position = L.marker(latlng).addTo(map);
+
+    current_accuracy = L.circle(latlng, radius).addTo(map);
+
+    if (currentlyTracking) {
+        map.setView(latlng);
+    }
+}
+
+function onLocationError(e) {
+    console.error("Location found error");
+}
+
+// geolocation
+
+navigator.geolocation.watchPosition(onLocationFound, onLocationError, {
+    maximumAge: 200,
+    timeout: 5000,
+    enableHighAccuracy: true
+});
+  
--- todo.md
+++ todo.md
@@ -1,3 +1,5 @@
+=> [] Attempt to locate users automatically with location permission. Make sure to make clear that if initially placed marker is incorrect, user should type in their location by hand.
+
 - [ ] Make basic UI
 - [ ] Connect APIS (Google Maps, FEMA, etc)
 - [ ] Search bar/autofill dropdown to select type of emergency
