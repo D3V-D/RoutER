cb3397ecc63a7e94d599402a163b680f57d5abd2
Alex3832 <93358123+Alex3832@users.noreply.github.com>
2023-10-20 17:54:44 -0400

    index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
+var search_terms = ['hurricane', 'tsunami', 'earthquake', 'hospital', 'fire', 'tornado', 'blizzard', 'homeless', 'food', flood', 'sandstorm', 'shelter', 'post-disaster'];
 
+function autocompleteMatch(input) {
+  if (input == '') {
+    return [];
+  }
+  var reg = new RegExp(input)
+  return search_terms.filter(function(term) {
+	  if (term.match(reg)) {
+  	  return term;
+	  }
+  });
+}
+
+function showResults(val) {
+  res = document.getElementById("result");
+  res.innerHTML = '';
+  let list = '';
+  let terms = autocompleteMatch(val);
+  for (i=0; i<terms.length; i++) {
+    list += '<li>' + terms[i] + '</li>';
+  }
+  res.innerHTML = '<ul>' + list + '</ul>';
+}
