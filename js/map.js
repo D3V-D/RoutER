96fdf70ad96ef8a374f2064008cbeb72bb5182fd
D3V-D <devendangi10@gmail.com>
2023-10-21 14:58:10 -0400

    Route now starts at user location. Updated tracking option. Waypoints aren't draggable. Added alt options section (currently blank).

--- js/map.js
+++ js/map.js
@@ -4,19 +4,10 @@
 
 var map = L.map('map').setView([30.4380832, -84.2809332], 13);
 
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-L.Routing.control({
-    waypoints: [
-        L.latLng(30.4380832, -84.2809332),
-        L.latLng(30.45, -84.3)
-    ],
-    // show: false,
-    addWaypoints: false,
-    routeWhileDragging: false
 }).addTo(map);
 
 L.control.scale().addTo(map);
@@ -27,11 +18,26 @@ L.Control.Track = L.Control.extend({
 
         trackingButton.innerHTML = "⮙";
         trackingButton.href = '#';
+        trackingButton.style.color = "black"
         trackingButton.role = 'button';
         trackingButton.label = 'Track';
         trackingButton.setAttribute('title', "Track");
         trackingButton.setAttribute('aria-label', 'Track');
         trackingButton.setAttribute('aria-disabled', 'false');
+
+        // button funcitonality
+        trackingButton.addEventListener("click", ()=> {
+            currentlyTracking = currentlyTracking ? false : true;
+            if (currentlyTracking) {
+                trackingButton.style.background = "aliceblue"
+                trackingButton.style.color = "blue"
+                trackingButton.setAttribute('title', "Stop Tracking");
+            } else {
+                trackingButton.style.background = "white"
+                trackingButton.style.color = "black"
+                trackingButton.setAttribute('title', "Track");
+            }
+        })
 
         return trackingButton
     },
@@ -50,6 +56,7 @@ L.control.track({ position: 'topleft' }).addTo(map);
 var current_position, current_accuracy;
 
 var initialGeolocation = true;
+var initialGeolocationError = true;
 var currentlyTracking = false;
 
 function onLocationFound(e) {
@@ -67,30 +74,45 @@ function onLocationFound(e) {
         lng: e.coords.longitude
     }
 
-    
-
-    current_position = L.marker(latlng).addTo(map);
-
-    current_accuracy = L.circle(latlng, radius).addTo(map);
 
     if (currentlyTracking) {
         // if in tracking mode.
         map.setView(latlng);
+        map.setZoom(17);
+        routingControl.setWaypoints([
+            L.latLng(latlng.lat, latlng.lng),
+            L.latLng(30.4380832, -84.2809332)
+        ])
     }
 
     if (initialGeolocation) {
         initialGeolocation = false;
+        initialGeolocationError = false;
         map.setZoom(17);
-        alert("Location may be inaccurate; if so, please enter start address manually. Please wait a few seconds first.");
         setTimeout(() => {
             map.setView(latlng);
-        }, 300);
-        console.log(latlng)
+        }, 1000);
+        alert("Location may be inaccurate; if so, please enter start address manually.")
+        routingControl = L.Routing.control({
+            waypoints: [
+                L.latLng(latlng.lat, latlng.lng),
+                L.latLng(30.4380832, -84.2809332),
+            ],
+            draggableWaypoints: false,
+            addWaypoints: false,
+            routeWhileDragging: false,
+        }).addTo(map);
+        current_accuracy = L.circle(latlng, radius).addTo(map);
     }
 }
 
 function onLocationError(e) {
     console.error("Location found error");
+
+    if (initialGeolocationError) {
+        initialGeolocationError = false;
+        alert("Delay in locating user; please wait a few more seconds or manually enter start location.");
+    }
 }
 
 // geolocation
